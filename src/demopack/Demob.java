128d5b50fc50e7308564fea9daa0f7aa3d1f4549
swatisonal91214 <swatisonal14@gmail.com>
2022-02-27 00:06:11 +0530

    Program explaining methods

---
+++ src/demopack/Demob.java
@@ -0,0 +1,17 @@
+package demopack;
+
+public class Demob {
+
+	public static void main(String[] args) {
+		
+		Demob obj = new Demob();
+		int product = obj.multiply(5,6);
+		System.out.println(product);
+		
+	}
+	
+	public int multiply(int num1, int num2) {
+		return (num1*num2); 
+	}
+
+}
