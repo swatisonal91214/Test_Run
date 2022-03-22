acc30455411c9ccbb8c507889aaee6729de51f6d
Durgesh094 <durgeshchavan1994@gmail.com>
2022-03-22 20:46:02 +0530

    Demo Remote Repo Operation.

--- src/com/bridgelabz/demo/Democlass.java
+++ src/com/bridgelabz/demo/Democlass.java
@@ -3,5 +3,6 @@ package com.bridgelabz.demo;
 public class Democlass {
     public static void main(String[] args) {
         System.out.println(" Demo Repository Operations.....");
+        System.out.println("");
     }
 }
--- src/com/bridgelabz/demo/Main.java
+++ src/com/bridgelabz/demo/Main.java
@@ -3,5 +3,6 @@ package com.bridgelabz.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Branching Operations....");
+        System.out.println("");
     }
 }
